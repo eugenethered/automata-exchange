9075a43a0044cb55321f94605922a1a477bfb28e
Eugene the Red <me@eugenethered.com>
2022-04-29 14:32:28 +0100

    using filtering to graph, rather than partial

--- exchange/rate/ExchangeRateHolder.py
+++ exchange/rate/ExchangeRateHolder.py
@@ -23,13 +23,14 @@ class ExchangeRateHolder:
     def get(self, instrument=None):
         if instrument is None:
             return self.exchange_rates
-        return self.exchange_rates[instrument]
+        if instrument not in self.exchange_rates:
+            return None
+        return dict([(k, v) for k, v in self.exchange_rates.items() if k == instrument])
 
-    def get_instrument_rate(self, instrument, instrument_to, instant):
+    def get_rate(self, instrument, instrument_to, instant):
         if instrument not in self.exchange_rates or instrument_to not in self.exchange_rates[instrument]:
             return None
         return next(ir.rate for ir in self.exchange_rates[instrument][instrument_to] if ir.instant == instant)
 
-    def get_instrument_rates(self, instrument, to_instrument):
+    def get_rates(self, instrument, to_instrument):
         return self.exchange_rates[instrument][to_instrument]
-
